e884e717c6eea61df58240d35b1c85c33b5243cf
matthsworld <matthsworld@gmail.com>
2022-07-06 16:24:51 +0800

    Redo

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
-""" Module that contains a function that returns the number of lines
-    of a text file
-"""
+''' function that returns the number of lines of a text file
+'''
 
 
 def number_of_lines(filename=""):
-    """ Function that reads from a file and prints its number of lines
-
-    Args:
-        filename: filename
-
-    Raises
-        Exception: when the file can be opened
-
-    """
-    n_lines = 0
-    with open(filename, 'r', encoding="utf-8") as f:
+    ''' function: number_of_lines
+    '''
+    if filename == "" or type(filename) != str:
+        return 0
+    counter = 0
+    with open(filename, 'r') as f:
         for line in f:
-            n_lines += 1
-    return n_lines
+            counter += 1
+    return counter
--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python3
-""" Module that contains a function that reads n lines of a text file
-"""
+'''  function that reads n lines of a text file (UTF8) and prints it to stdout
+'''
 
 
 def read_lines(filename="", nb_lines=0):
-    """ Function that reads from a file and prints its number of lines
-
-    Args:
-        filename: filename
-        nb_lines: number of lines to print
-
-    Raises
-        Exception: when the file can be opened
-
-    """
-
-    with open(filename, 'r', encoding="utf-8") as f:
-        if nb_lines <= 0:
-            read_data = f.read()
-            print(read_data, end='')
-        else:
-            n_lines = 0
-            for line in f:
+    ''' function: read_lines
+    '''
+    if filename == "" or type(filename) != str:
+        return
+    if type(nb_lines) != int:
+        return
+    counter = 0
+    with open(filename, "r") as f:
+        for line in f:
+            counter += 1
+            if nb_lines <= 0 or (counter <= nb_lines and nb_lines > 0):
                 print(line, end='')
-                n_lines += 1
-                if n_lines == nb_lines:
-                    break
+            else:
+                break
